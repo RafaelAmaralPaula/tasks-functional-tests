6fa1c1b83098fec69819a69c612cab8e3475ed59
Rafael Amaral de Paula <rafael.paula@sysmap.com.br>
2022-12-13 15:40:05 -0300

    update functional test

--- src/test/java/br/ce/wc/aquibo/tasks/functionaltest/TasksTest.java
+++ src/test/java/br/ce/wc/aquibo/tasks/functionaltest/TasksTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
